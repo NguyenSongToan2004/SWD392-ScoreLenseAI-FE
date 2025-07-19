6f15614ce2357372fafd6856cddf4a2e3f4187dd
Nguyễn Song Toàn <toannsse183104@fpt.edu.vn>
2025-07-19 09:59:38 +0700

    Add firebase config and update .gitignore
    Added public/firebase-config.js with Firebase configuration for service worker. Removed firebase-config.js from .gitignore to allow tracking this file.

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+// Config cho service worker
+const firebaseConfig = {
+  apiKey: "AIzaSyAfuU9PbNAs5j-jOHMi7wIFq7VSy0-sQD4", // Thay bằng key thật
+  authDomain: "scorelen.firebaseapp.com",
+  projectId: "scorelen", 
+  storageBucket: "scorelen.firebasestorage.app",
+  messagingSenderId: "282874284638",
+  appId: "1:282874284638:web:cc02d0ac32837e8a74c2cf"
+};
